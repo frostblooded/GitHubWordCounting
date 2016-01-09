441142adb75ea35bfa8c1e0d3130bced42ba4799
frostblooded <frostblooded@yahoo.com>
2016-01-09 16:21:27 +0200

    adding files we already have

---
+++ main.rb
@@ -0,0 +1,61 @@
+require 'word_counter'
+
+def uri?(string)
+  start = string.split('/').first
+  start == 'http:' || start == 'https:'
+end
+
+def get_result(input)
+  if File.file? input
+    WordCounter.parse_file input
+  elsif uri? input
+    WordCounter.parse_webpage input
+  else
+    WordCounter.parse input
+  end
+end
+
+def write_to_file(file, data)
+  File.open(file, 'w') do |f|
+    f << data
+  end
+end
+
+def print_result(result, format)
+  if format == 'json'
+    puts result.to_json
+  elsif format == 'xml'
+    puts result.to_xml
+  else
+    puts result.to_csv
+  end
+
+  svg = result.to_svg
+  write_to_file('A_21_Nikolay_Danailov.svg', svg)
+end
+
+def iterate_folder(folder)
+  files = Dir.glob("#{folder}/**/*").select { |f| File.file? f }
+  result = WordCounter::Result.new
+
+  files.each do |file|
+    temp = WordCounter.parse_file file
+    result.word_counts.merge!(temp.word_counts.to_h) { |_, oldval, newval| newval + oldval }
+    result.marks_count += temp.marks_count
+  end
+
+  result.word_counts = result.word_counts.sort_by { |word, count| [-count, word] }.to_h
+  result
+end
+
+if ARGV[0] == '-d'
+  folder = ARGV[1]
+  format = ARGV[2]
+  result = iterate_folder folder
+  print_result result, format
+else
+  input = ARGV[0]
+  output_format = ARGV[1]
+  result = get_result input
+  print_result result, output_format
+end
